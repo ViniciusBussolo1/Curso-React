150fc41fac76713655c7dc77a1e5f8a924dc1af6
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-09-23 10:50:51 -0300

    Button Parte 1: Ajustando para aceitar polimorfismo "as"

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,27 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
+
 import * as S from './styles'
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
 export type ButtonProps = {
-  children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: JSX.Element
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+} & ButtonTypes
 
 const Button = ({
   children,
   icon,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
-    {!!icon && icon}
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
