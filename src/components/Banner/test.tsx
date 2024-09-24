fb792119919ad11b18d76b97fdb1a884928538c1
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-09-24 10:16:50 -0300

    Banner Parte 2: Escrevendo testes

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -3,22 +3,26 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    renderWithTheme(
-      <Banner
-        title="Defy death"
-        subtitle="Play the new CrashLands season"
-        img="https://source.unsplash.com/user/willianjusten/1042x580"
-        buttonLabel="Buy now"
-        buttonLink="/games/defy-death"
-      />
-    )
-    //verifique se o title existe renderizado
-    expect(screen.getByLabelText(/defy death/i)).toBeInTheDocument()
-    //verifique se o subtitle existe renderizado
+    renderWithTheme(<Banner {...props} />)
 
-    //verifique se a imagem existe renderizado
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /play the new crashlands season/i })
+    ).toBeInTheDocument()
+
     expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
   })
 })
