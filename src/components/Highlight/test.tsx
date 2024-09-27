a27ec2d21dda77c82f41cc63b05ed0bcb8c0354a
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-09-27 09:38:43 -0300

    Highlight Parte 2: Criando a estrutura e testes

---
+++ src/components/Highlight/test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Highlight from '.'
+
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  buttonLabel: 'buy Now',
+  buttonLink: '/rdr2'
+}
+
+describe('<Highlight />', () => {
+  it('should render heading and buttons', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /heading 1/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /heading 2/i })
+    ).toBeInTheDocument()
+  })
+})
