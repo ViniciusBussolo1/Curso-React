f9efbb266f3f500bb35c06e9842a83cf9c2356be
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-09-27 10:06:05 -0300

    Highlight Parte 4: Adicionando opção de Float Image

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,10 +1,12 @@
 import Button from 'components/Button'
 import * as S from './styles'
+import { title } from 'process'
 
 export type HighlightProps = {
   title: string
   subtitle: string
   backgroundImage: '/img/red-dead-img.jpg'
+  floatImage?: string
   buttonLabel: string
   buttonLink: string
 }
@@ -13,10 +15,12 @@ const Highlight = ({
   title,
   subtitle,
   backgroundImage,
+  floatImage,
   buttonLabel,
   buttonLink
 }: HighlightProps) => (
   <S.Wrapper backgroundImage={backgroundImage}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
     <S.Content>
       <S.Title>{title}</S.Title>
       <S.Subtitle>{subtitle}</S.Subtitle>
