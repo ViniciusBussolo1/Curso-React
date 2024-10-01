663e83d4090ec710dccfea3d6b6aa1028c88824a
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-10-01 10:14:29 -0300

    GameCard Parte 3: Solução dos testes mais CSS já criado

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import GameCard, { GameCardProps } from '.'
+import { Default } from 'components/Banner/stories'
 export default {
   title: 'GameCard',
   component: GameCard,
@@ -7,7 +8,13 @@ export default {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
-    price: 'R$ 235,00'
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
   }
 } as Meta
 export const Default: Story<GameCardProps> = (args) => (
