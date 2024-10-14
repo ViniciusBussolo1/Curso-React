de8c3066881c6f52f33ad06703c8208f50dd1792
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-10-14 10:11:20 -0300

    Home Parte 4: Criando backgrounds customizados para o Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,20 @@
-import { RouterContext } from 'next/dist/shared/lib/router-context'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const parameters = {
-  nextRouter: {
-    Provider: RouterContext.Provider
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
   }
 }
 
